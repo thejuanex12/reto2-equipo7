e53f07bedc50d42346f80265f880c1f4d610bada
Juan Esteban Cañas <juanex12@gmail.com>
2022-09-27 19:41:09 -0500

    added Spring files

---
+++ src/test/java/com/example/Team7/Team7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
